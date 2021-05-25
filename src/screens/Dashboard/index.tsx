3f653fba473c4fcdef0efd31f5077e8b874cd7c1
JordaoViktor <jordaovpinheiro@gmail.com>
2021-05-25 00:09:41 -0300

    feat: creating project and add global styles

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { Container, Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  );
+}
+
+export default Dashboard;
